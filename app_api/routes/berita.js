35a70ae2a5c072772b950108b13531960ad39138
urano <darrenlowell_2226240033@mhs.mdp.ac.id>
2024-12-17 16:54:01 +0700

    remove auth berita for user

--- app_api/routes/berita.js
+++ app_api/routes/berita.js
@@ -8,12 +8,12 @@ const authmiddleware = require("../middleware/authMiddleware");
 const roleMiddleware = require("../middleware/roleMiddleware");
 
 // Definisi rute untuk berita
-router.get("/", authmiddleware, beritaController.getAllBerita);
+router.get("/", beritaController.getAllBerita);
 
 // Route untuk membuat berita baru dengan upload gambar
 router.post("/", authmiddleware, roleMiddleware( "admin"), upload.single('gambarUtama'), beritaController.createBerita);
 
-router.get("/:id", authmiddleware, beritaController.getBeritaById);
+router.get("/:id", beritaController.getBeritaById);
 
 // Route untuk update berita, dengan middleware upload untuk menangani gambar baru (jika ada)
 router.put("/:id", authmiddleware, roleMiddleware( "admin"), upload.single('gambarUtama'), beritaController.updateBerita); 
