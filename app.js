5e9f6ff22dd6fe64b31e1668b659ff2b07ec0acd
urano <darrenlowell_2226240033@mhs.mdp.ac.id>
2024-12-17 15:40:18 +0700

    fixing auth app.js

--- app.js
+++ app.js
@@ -9,11 +9,12 @@ const connectDB = require("./app_api/models/db");
 
 
 const indexRouter = require('./app_server/routes/index');
-
 const beritaRouterApi = require("./app_api/routes/berita"); 
 const kategoriRouterApi = require("./app_api/routes/kategori");
 const penulisRouterApi = require("./app_api/routes/penulis");
 const sumberRouterApi = require("./app_api/routes/sumber");
+
+const authRouterApi = require("./app_api/routes/auth");
 
 require("dotenv").config();
 
@@ -40,6 +41,8 @@ app.use("/api/berita", beritaRouterApi);
 app.use("/api/kategori", kategoriRouterApi);
 app.use("/api/penulis", penulisRouterApi);
 app.use("/api/sumber", sumberRouterApi);
+
+app.use("/api/auth", authRouterApi);
 
 // connect to mongoDB
 connectDB();
