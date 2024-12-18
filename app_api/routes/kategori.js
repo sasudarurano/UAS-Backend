5b86888faf897da056c13e444a44cc53808e175e
urano <darrenlowell_2226240033@mhs.mdp.ac.id>
2024-12-18 10:37:22 +0700

    up post public

--- app_api/routes/kategori.js
+++ app_api/routes/kategori.js
@@ -11,7 +11,8 @@ const roleMiddleware = require("../middleware/roleMiddleware");
 // Definisi rute untuk kategori (diganti dari fakultas)
 router.get("/",  kategoriController.getAllKategori); // Ganti controller 
 
-router.post("/", authmiddleware, roleMiddleware("admin"), kategoriController.createKategori); // Ganti controller dan middleware
+// router.post("/", authmiddleware, roleMiddleware("admin"), kategoriController.createKategori); // Ganti controller dan middleware
+router.post("/", kategoriController.createKategori);
 
 router.get("/:id", kategoriController.getKategoriById); // Ganti controller
 
